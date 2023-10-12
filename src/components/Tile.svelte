788b1548c04a9cc92552230cf5c3cb17192080ea
angst <angst>
2023-10-12 04:48:04 +0200

    add ability to change color

--- src/components/Tile.svelte
+++ src/components/Tile.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
 	import { colors } from "$lib/config"
 	import type { Tile } from "$lib/game"
+	import { selected } from "$lib/store"
 
 	export let tile: Tile
 </script>
 
 
 	<button
-		on:click
+		on:click={()=>{
+			tile.type = $selected
+		}}
 		style="background-color: {colors[tile.type]};"
-		class="w-14 h-14 bg-orange-500">
+		class="w-14 h-14">
 	</button>
