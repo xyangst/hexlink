31c953f6f9b86ce74ed43868110ed3dfc854ea85
angst <angst>
2023-10-12 20:12:20 +0200

    forgot to undo dev change

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -3,7 +3,7 @@ import { writable } from "svelte/store"
 export const moves = writable(0)
 export const board = writable(generateRandomBoard())
 export const selected = writable(2)
-export const modalState = writable<"gameover" | "help" | "none">("gameover")
+export const modalState = writable<"gameover" | "help" | "none">("none")
 export function resetGame() {
 	moves.set(0)
 	board.set(generateRandomBoard())
